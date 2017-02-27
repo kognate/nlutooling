f58edf85b3a1d4e9dad062e19a6e8128502fe024
Joshua B. Smith <joshua.b.smith@us.ibm.com>
2017-02-27 12:43:11 -0500

    added new api module

---
+++ post_parameter.h
@@ -0,0 +1,16 @@
+//
+// Created by Josh Smith on 2/26/17.
+//
+
+#ifndef WATSON_POST_PARAMETER_H
+#define WATSON_POST_PARAMETER_H
+
+
+
+class post_parameter {
+
+};
+
+
+
+#endif //WATSON_POST_PARAMETER_H
---
+++ vision.h
@@ -0,0 +1,16 @@
+//
+// Created by Josh Smith on 2/26/17.
+//
+
+#ifndef WATSON_VISION_H
+#define WATSON_VISION_H
+
+
+
+class vision {
+
+};
+
+
+
+#endif //WATSON_VISION_H
